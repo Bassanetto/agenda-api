f37f3308bb696b030da30bb28ce62b7bd88cc0df
“Bassanetto” <gustavo.almeidabassanetto@gmail.com>
2024-04-23 17:29:03 -0300

    Agenda API :: Implementação de Commands, Handlers, Repository no projeto

---
+++ src/Services/Evento/Evento.Application/Configurations/DataBaseConfiguration.cs
@@ -0,0 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Evento.Application.Configurations;
+
+public static class DataBaseConfiguration
+{
+    public static void ConfigureDataBaseEventos(this IServiceCollection services, string defaultConnection)
+    {
+        services.AddDbContext<EventosContext>(options => options.UseSqlServer(defaultConnection));
+    }
+
+    public static void UseDataBaseEventos(this IServiceScope serviceScope)
+    {
+        var context = serviceScope.ServiceProvider.GetRequiredService<EventosContext>();
+        context.Database.Migrate();
+    }
+}
